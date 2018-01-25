805ed10b85e2c418949202cb21048be8c9031d00
mengxumx <mengxumx@cn.ibm.com>
2018-01-25 03:53:30 -0600

    get ready for npm

--- binding.gyp
+++ binding.gyp
@@ -1,22 +1,6 @@
 {
   'targets': [
     {
-      'cflags': [
-        '-std=c++0x',
-        '-Wno-unknown-pragmas',
-        '-Wno-format',
-        '-O0'
-      ],
-      'ldflags': [
-        '-static-libstdc++', 
-        '-static-libgcc'
-      ],
-      'link_settings': {
-        'libraries': [
-          '-L/usr/lib/libiconv.a',
-          '-ldb400'
-        ],
-      },
       'target_name': 'db2ia',
       'include_dirs': [
         'src/db2ia',
@@ -26,7 +10,34 @@
         'src/db2ia/db2ia.cc', 
         'src/db2ia/dbconn.cc',
         'src/db2ia/dbstmt.cc'
-      ]
+      ],
+      'cflags': [
+        '-std=c++0x',
+        '-Wno-unknown-pragmas',
+        '-Wno-format',
+        '-gxcoff',
+        '-O0'
+      ],
+      'conditions': [
+        [ 'target_arch=="ppc"', {
+          'ldflags': [ '-Wl,-bmaxdata:0x60000000/dsa' ],
+        }],
+        [ 'target_arch=="ppc64"', {
+          'cflags': [ '-maix64' ],
+          'ldflags': [ '-maix64' ],
+        }],
+      ],
+      'ldflags': [ 
+        '-Wl,-bbigtoc', 
+        '-Wl,-brtl', 
+        '-Wl,-blibpath:/QOpenSys/pkgs/lib:/QOpenSys/usr/lib:/opt/freeware/lib'
+      ],
+      'link_settings': {
+        'libraries': [
+          '-L/QOpenSys/pkgs/lib:/QOpenSys/usr/lib:/opt/freeware/lib',
+          '-ldb400'
+        ],
+      }
     }
   ]
 }
--- test/test.js
+++ test/test.js
@@ -1,147 +1,62 @@
-var dba = require('./db2a');
+var sql = 'SELECT STATE FROM QIWS.QCUSTCDT';
 
-// The new DB2 driver can reuse one connection with multiple queries.
-var conn = new dba.dbconn();
-conn.init(function(){
-  conn.conn("*LOCAL");
+var dba = require('../lib/db2a');
+var dbconns = new dba.dbconn();
+// dbconns.debug(true);
+dbconns.conn("*LOCAL");
+var sqlAs = new dba.dbstmt(dbconns);
+var sqlBs = new dba.dbstmt(dbconns);
+console.log("Execute A.");
+sqlAs.execSync(sql, function(x) {
+  console.log("Execute A Done. Row Count: %d", x.length);
+  sqlAs.close();
 });
-    
-function testQureySyncI(sql) {
-  console.log("\n  testQureySyncI() ----------------------------------");
-  stmt = new dba.dbstmt(conn);
+console.log("Execute B.");
+sqlBs.execSync(sql, function(x) {
+  console.log("Execute B Done. Row Count: %d", x.length);
+  sqlBs.close();
+});
+dbconns.disconn();
+dbconns.close();
 
-  console.log("Executing statement A");
-  stmt.execSync(sql, function() {
-    console.log("Statement A is done.");
+console.log("Test Async in callback.");
+var dbconn = new dba.dbconn();
+// dbconn.debug(true);
+dbconn.conn("*LOCAL");
+var sqlA = new dba.dbstmt(dbconn);
+var sqlB = new dba.dbstmt(dbconn);
+console.log("Execute A.");
+sqlA.exec(sql, function(x) {
+  console.log("Execute A Done. Row Count: %d", x.length);
+  sqlA.close();
+  console.log("Execute B.");
+  sqlB.exec(sql, function(x) {
+    console.log("Execute B Done. Row Count: %d", x.length);
+    sqlB.close();
+    dbconn.disconn();
+    dbconn.close();
   });
-  console.log("Executing statement B");
-  stmt.execSync(sql, function() {
-    console.log("Statement B is done.");
+});
+
+setTimeout(function(){
+  console.log("Test Async concurrently.");
+  var dbconn = new dba.dbconn();
+  // dbconn.debug(true);
+  dbconn.conn("*LOCAL");
+  var sqlA = new dba.dbstmt(dbconn);
+  var sqlB = new dba.dbstmt(dbconn);
+  console.log("Execute A.");
+  sqlA.exec(sql, function(x) {
+    console.log("Execute A Done. Row Count: %d", x.length);
+    sqlA.close();
   });
-}
-
-function testQureyAsyncI(sql) {
-  console.log("\n  testQureyAsyncI() ----------------------------------");
-
-  stmtA = new dba.dbstmt(conn);
-  stmtB = new dba.dbstmt(conn);
-  
-  console.log("Executing statement A");
-  stmtA.exec(sql, function() {
-    console.log("Statement A is done.");
+  console.log("Execute B.");
+  sqlB.exec(sql, function(x) {
+    console.log("Execute B Done. Row Count: %d", x.length);
+    sqlB.close();
   });
-  console.log("Executing statement B");
-  stmtB.exec(sql, function() {
-    console.log("Statement B is done.");
-  });
-}
-
-function testQureySyncIII(sql) {
-	console.log("\n  testQureySyncIII() ----------------------------------");
-	try{
-    stmt = new dba.dbstmt(conn);
-		stmt.prepareSync(sql, function() {
-      stmt.executeSync(function() {
-        var fieldNum = stmt.numFields();
-        console.log("There are %d fields in each row.\n", fieldNum);
-        console.log("Name | Length | Type | Precise | Scale | Null");
-        for(var i = 0; i < fieldNum; i++)
-          console.log("%s | %d | %d | %d | %d | %d", stmt.fieldName(i), stmt.fieldWidth(i), stmt.fieldType(i), stmt.fieldPrecise(i), stmt.fieldScale(i), stmt.fieldNullable(i));
-        console.log("There are %d rows affected.", stmt.numRows());
-        stmt.fetchAllSync(function(rs) {
-          console.log("Result is : %s", JSON.stringify(rs));
-        });
-      });
-    });
-	} catch(e) {
-		console.log(e);
-	}
-};
-
-function testQureyAsyncIII(sql, cb) {
-	console.log("\n  testQureyAsyncIII() ----------------------------------");
-	try{
-    stmt = new dba.dbstmt(conn);
-		stmt.prepare(sql, function() {
-      stmt.execute(function() {
-        var fieldNum = stmt.numFields();
-        console.log("There are %d fields in each row.\n", fieldNum);
-        console.log("Name | Length | Type | Precise | Scale | Null");
-        for(var i = 0; i < fieldNum; i++)
-          console.log("%s | %d | %d | %d | %d | %d", stmt.fieldName(i), stmt.fieldWidth(i), stmt.fieldType(i), stmt.fieldPrecise(i), stmt.fieldScale(i), stmt.fieldNullable(i));
-        console.log("There are %d rows affected.", stmt.numRows());
-        stmt.fetchAll(function(rs) {
-          console.log("Result is : %s", JSON.stringify(rs));
-          cb();
-        });
-      });
-    });
-	} catch(e) {
-		console.log(e);
-	}
-};
-
-function testQureySyncV(sql) {
-  console.log("\n  testQureySyncV() ----------------------------------");
-	var ipc = "*NA";
-	var ctl = "*here";
-	var xmlIn ="<?xml version='1.0'?><script><comment>your XML script here, cmd, sh, sql, pgm (below)</comment></script>";
-	var xmlOut = "";
-	try{
-    stmt = new dba.dbstmt(conn);
-		stmt.prepareSync(sql, function(){
-      stmt.bindParamSync([
-        [ipc, dba.SQL_PARAM_INPUT, 1],
-        [ctl, dba.SQL_PARAM_INPUT, 1],
-        [xmlIn, dba.SQL_PARAM_INPUT, 0],
-        [xmlOut, dba.SQL_PARAM_OUTPUT, 0],
-      ], function(){
-        stmt.executeSync(function(out) {  //out is an array of the output parameters.
-          for (i=0; i<out.length; i++)
-            console.log("Output Param[%d] = %s \n", i, out[i]);
-        });
-      });
-    });
-	} catch(e) {
-		console.log(e);
-	}
-};
-
-function testQureyAsyncV(sql, cb) {
-  console.log("\n  testQureyAsyncV() ----------------------------------");
-	var ipc = "*NA";
-	var ctl = "*here";
-	var xmlIn ="<?xml version='1.0'?><script><comment>your XML script here, cmd, sh, sql, pgm (below)</comment></script>";
-	var xmlOut = "";
-	try{
-    stmt = new dba.dbstmt(conn);
-		stmt.prepare(sql, function(){
-      stmt.bindParam([
-        [ipc, dba.SQL_PARAM_INPUT, 1],
-        [ctl, dba.SQL_PARAM_INPUT, 1],
-        [xmlIn, dba.SQL_PARAM_INPUT, 0],
-        [xmlOut, dba.SQL_PARAM_OUTPUT, 0],
-      ], function(){
-        stmt.execute(function(out) {  //out is an array of the output parameters.
-          for (i=0; i<out.length; i++)
-            console.log("Output Param[%d] = %s \n", i, out[i]);
-          cb();
-        });
-      });
-    });
-	} catch(e) {
-		console.log(e);
-	}
-};
-
-// testQureySyncI("SELECT STATE FROM QIWS.QCUSTCDT");
-// testQureySyncIII("SELECT STATE FROM QIWS.QCUSTCDT");
-// testQureySyncV("call QXMLSERV.iPLUG512K(?,?,?,?)");
-
-// testQureyAsyncI("SELECT STATE FROM QIWS.QCUSTCDT");
-// testQureyAsyncIII("SELECT STATE FROM QIWS.QCUSTCDT", function(){
-  // testQureyAsyncV("call QXMLSERV.iPLUG512K(?,?,?,?)", function(){});
-// });
-
-testQureyAsyncIII("SELECT STATE FROM QIWS.QCUSTCDT", function(){});
-testQureyAsyncIII("SELECT LSTNAM FROM QIWS.QCUSTCDT", function(){});
+  setTimeout(function(){
+    dbconn.disconn();
+    dbconn.close();
+  },1000); //Sleep for 1 sec to wait for both executions done before disconnecting.
+},1000); //Sleep for 1 sec to wait for previous test done.
